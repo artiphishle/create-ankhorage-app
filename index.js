0f8c8b682c232679958553e4dbed95e13d0e534b
artiphishle <fabio.gartenmann@protonmail.com>
2024-11-14 07:11:13 +0100

    Fix bug

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 const { execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
+const prompts = require("@inquirer/prompts");
 
 const createApp = async () => {
   // Step 1: Ask for the app name
-  const { appName } = await require("@inquirer/prompts").input({
+  const appName = await prompts.input({
     type: "text",
     name: "appName",
     message: "Enter the name of the app:",
