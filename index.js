2c4071294d7b843982bacad978f3b9169da3465a
artiphishle <fabio.gartenmann@protonmail.com>
2024-11-16 02:41:57 +0100

    Update ENV validation

--- index.js
+++ index.js
@@ -2,25 +2,19 @@
 
 const { execSync } = require("child_process");
 const prompts = require("@inquirer/prompts");
+const boilerplate = "https://github.com/artiphishle/ankh-native-app.git";
 
 const writeTitle = () => {
   execSync("echo", { stdio: 'inherit' });
   execSync("echo A N K H O R A G E", { stdio: 'inherit' });
   execSync("echo - - - - - - - - -", { stdio: 'inherit' });
   execSync("echo", { stdio: 'inherit' });
+  execSync("echo", { stdio: 'inherit' });
+  execSync("Info: echo Make sure ENV's are set: 'AMPLIFY_ACCESS_KEY_ID' & 'AMPLIFY_SECRET_ACCESS_KEY'")
 };
 
 const createApp = async () => {
   writeTitle();
-
-  if (!process.env.AMPLIFY_ACCESS_KEY_ID) {
-    execSync("echo Missing env: AMPLIFY_ACCESS_KEY_ID");
-    process.exit(1);
-  }
-  if (!process.env.AMPLIFY_SECRET_ACCESS_KEY) {
-    execSync("echo Missing env: AMPLIFY_SECRET_ACCESS_KEY");
-    process.exit(1);
-  }
 
   // Step 1: Ask for the app name
   const appName = await prompts.input({
@@ -31,7 +25,7 @@ const createApp = async () => {
   });
 
   // Step 2: Clone Native App
-  execSync(`git clone https://github.com/artiphishle/ankh-native-app.git ${appName}`);
+  execSync(`git clone ${boilerplate} ${appName}`);
 
   // Step 3: Install App, Amplify, Cognito & publish
   execSync(`npm i && amplify init && amplify add auth && amplify push && amplify hosting && amplify publish`, { cwd: appName, stdio: 'inherit' });
