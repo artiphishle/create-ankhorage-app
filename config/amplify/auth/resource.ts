3b227ce5adf2c5cb2d43ec19a9da20465c32dbba
artiphishle <fabio.gartenmann@protonmail.com>
2024-11-21 02:55:12 +0100

    Add custom fields for signUp

--- config/amplify/auth/resource.ts
+++ config/amplify/auth/resource.ts
@@ -1,7 +1,22 @@
 import { defineAuth } from '@aws-amplify/backend';
 
 export const auth = defineAuth({
+
   loginWith: {
     email: true
+  },
+  userAttributes: {
+    'custom:firstName': {
+      dataType: "String",
+      mutable: true,
+      minLen: 2,
+      maxLen: 25
+    },
+    'custom:lastName': {
+      dataType: "String",
+      mutable: true,
+      minLen: 2,
+      maxLen: 25
+    },
   }
 });
